1eab5708c7f0e2dcc5e9ba14795424e4f4b44b93
Igidali <d3facerx@gmail.com>
2024-01-17 23:24:50 +0400

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,39 @@
+"use strict";
+// Types
+let d = 'hello'; // Accept all types
+let a = 12;
+let b = '4';
+let c; // Union defining
+function test(a, b) {
+    console.log(a / b);
+}
+// Array and Objects 
+const arrString = ['test1', 'test2', 'test3']; // Only string
+const arrUnion = [12345, 'test4']; // String or number (union)
+const arrMixed = [12345, 'test', true]; // Mixed array
+// arrString[1] = 45  // Not acceptable
+arrMixed[1] = 45;
+// Tuple
+const arrTuple = [12345, "test", true]; // Order is important
+// arrTuple[0] = 'test' // Not acceptable, 0-nth element accepts only string type
+// Objects
+let Jimmy = {
+    name: "Jimmy",
+    book: 1984,
+    dead: true
+};
+let George = {
+    name: "George",
+    book: 1984,
+    dead: true
+};
+function helloWriter(writer) {
+    return writer.name;
+}
+console.log(helloWriter(George));
+// Literal types
+let constant;
+// constant = 'new value' // "constant" accepts only "only me accapted" string
+let constantUnion;
+// let constantUnion: "Cucumber" // "constantUnion" accepts only one of three values
+// Functions
---
+++ src/main.ts
@@ -0,0 +1,80 @@
+// Types
+let d:any = 'hello' // Accept all types
+let a:number = 12
+let b:string = '4'
+let c : number | boolean // Union defining
+
+function test(a:number,b:number) {
+    console.log(a/b)
+}
+
+// Array and Objects 
+
+const arrString = ['test1','test2', 'test3'] // Only string
+
+const arrUnion = [12345, 'test4'] // String or number (union)
+
+const arrMixed = [12345, 'test', true] // Mixed array
+
+// arrString[1] = 45  // Not acceptable
+
+arrMixed[1] = 45
+
+// Tuple
+
+const arrTuple: [number,string,boolean] = [12345, "test", true] // Order is important
+
+// arrTuple[0] = 'test' // Not acceptable, 0-nth element accepts only string type
+
+
+// Objects
+
+
+let Jimmy: {name:string,book:number,dead:boolean} = {
+    name: "Jimmy",
+    book: 1984,
+    dead: true
+}
+
+
+// Type aliases
+
+type stringORnumber = string | number
+type stringORnumberArray = (string | number)[]
+
+// type or interface
+type  Writer = { 
+    name: string,
+    book?: number, // "?" making optional that property
+    dead: boolean
+}
+
+let George: Writer = {
+    name: "George",
+    book: 1984,
+    dead: true
+}
+
+function helloWriter (writer:Writer) {
+    return writer.name
+}
+
+console.log(helloWriter(George))
+
+// Literal types
+
+let constant: "Only me accepted"
+// constant = 'new value' // "constant" accepts only "only me accapted" string
+let constantUnion: "Banana" | "Apple" | "Kiwi"
+// let constantUnion: "Cucumber" // "constantUnion" accepts only one of three values
+
+// Functions
+
+
+
+
+
+
+
+
+
